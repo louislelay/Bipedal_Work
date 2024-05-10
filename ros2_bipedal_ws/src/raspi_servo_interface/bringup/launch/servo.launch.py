a020b73512ed6f793559bfad4da2d399b50cb892
Louis LE LAY <le.lay.louis@gmail.com>
2024-05-10 17:48:09 +0200

    up

---
+++ ros2_bipedal_ws/src/raspi_servo_interface/bringup/launch/servo.launch.py
@@ -0,0 +1,117 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_rviz",
+            default_value="false",
+            description="Does not start Rviz2 automatically with this launch file", # it's supposed to be launched inside raspberryPi
+        )
+    )
+    
+    # Initialize Arguments
+    use_rviz = LaunchConfiguration("use_rviz")
+    pkg_name = "raspi_servo_interface"
+    
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare(pkg_name),
+                    "urdf",
+                    "servo.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+    
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(pkg_name),
+            "config",
+            "servo_controllers.yaml"
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(pkg_name),
+         "config",
+         "config.rviz"
+        ]
+    )
+    
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        output="both",
+    )
+    
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(use_rviz),
+    )
+    
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delayed_rviz = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+    
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delayed_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+    
+    nodes = [
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        delayed_rviz,
+        delayed_robot_controller_spawner,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
+    
