dcc353ae2bee6b686cb47a465a0c0ff1fc0c5e1d
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-24 22:51:12 +0200

    Update test.py

--- ros2_bipedal_ws/src/test.py
+++ ros2_bipedal_ws/src/test.py
@@ -1,16 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO pins for encoder A and B outputs
-encoder_pin_A = 17
-encoder_pin_B = 27
-
-# Set up GPIO
+# Utiliser la numérotation BCM
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(encoder_pin_A, GPIO.IN)
-GPIO.setup(encoder_pin_B, GPIO.IN)
 
-while True:
-	print(GPIO.input(encoder_pin_A))
-	print(GPIO.input(encoder_pin_A))
-	time.sleep(0.1)
+# Liste des pins GPIO que nous allons vérifier
+gpio_pins = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
+
+# Configurer les GPIOs en entrée
+for pin in gpio_pins:
+    GPIO.setup(pin, GPIO.IN)
+
+# Lire et afficher les états des GPIOs
+try:
+    while True:
+        print("État des GPIOs:")
+        for pin in gpio_pins:
+            state = GPIO.input(pin)
+            print(f"GPIO {pin}: {'HIGH' if state else 'LOW'}")
+        # Ajouter une petite pause pour éviter d'inonder la console
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    # Nettoyer les configurations GPIO avant de sortir
+    GPIO.cleanup()
+    print("Nettoyage des GPIOs et arrêt du programme")
