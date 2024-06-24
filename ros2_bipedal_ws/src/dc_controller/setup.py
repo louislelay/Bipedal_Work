b233a8d5008d9f49650a602ed387a0443ef05746
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-25 00:08:19 +0200

    Update setup.py

--- ros2_bipedal_ws/src/dc_controller/setup.py
+++ ros2_bipedal_ws/src/dc_controller/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'dc_controller_node = dc_controller.dc_controller_node:main'
         ],
     },
 )
