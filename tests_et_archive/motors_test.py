1afbb1efc0ca5aa513d597d8ae796b8a69e54d57
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-25 07:43:49 +0200

    Update motors_test.py

--- tests_et_archive/motors_test.py
+++ tests_et_archive/motors_test.py
@@ -24,13 +24,51 @@ pwmB = GPIO.PWM(enB, 1000)
 pwmA.start(0)
 pwmB.start(0)
 
+def set_motor_a(speed, direction):
+    pwmA.ChangeDutyCycle(speed)
+    if direction == "forward":
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+    elif direction == "backward":
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
 
+def set_motor_b(speed, direction):
+    pwmB.ChangeDutyCycle(speed)
+    if direction == "forward":
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
+    elif direction == "backward":
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.HIGH)
 
-while True:
-	pwmA.ChangeDutyCycle(100)
-	pwmB.ChangeDutyCycle(100)
-	GPIO.output(in1, GPIO.HIGH)
-	GPIO.output(in2, GPIO.HIGH)
-	GPIO.output(in3, GPIO.HIGH)
-	GPIO.output(in4, GPIO.LOW)
-	time.sleep(1)
+def stop_motors():
+    pwmA.ChangeDutyCycle(0)
+    pwmB.ChangeDutyCycle(0)
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+
+try:
+    while True:
+        set_motor_a(75, "forward")
+        set_motor_b(75, "forward")
+        time.sleep(5)
+
+        set_motor_a(75, "backward")
+        set_motor_b(75, "backward")
+        time.sleep(5)
+
+        stop_motors()
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    stop_motors()
+    pwmA.stop()
+    pwmB.stop()
+    GPIO.cleanup()
+
