75d9ab1a5ed2a3a6ec79b7bbdd5f29aa31a37e8c
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-28 00:32:45 +0200

    Update balance_test.py

--- tests_et_archive/balance_test.py
+++ tests_et_archive/balance_test.py
@@ -128,7 +128,7 @@ try:
 
 		vit_mot, I, prev_input = PID(abs(roll), I, prev_input)
 
-		angless = [95, 90, 100, 85]
+		angless = [100, 90, 120, 85]
 
 		# Set servo positions based on angles
 		for i in range(4):
