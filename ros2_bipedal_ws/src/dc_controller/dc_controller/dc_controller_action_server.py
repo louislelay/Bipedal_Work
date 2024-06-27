47e323964aa8cf02772068447d4dfad2e061b9cb
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-27 15:53:59 +0200

    Update dc_controller_action_server.py

--- ros2_bipedal_ws/src/dc_controller/dc_controller/dc_controller_action_server.py
+++ ros2_bipedal_ws/src/dc_controller/dc_controller/dc_controller_action_server.py
@@ -1,200 +1,218 @@
 import rclpy
-from rclpy.action import ActionServer
+from rclpy.action import ActionServer, CancelResponse
 from rclpy.node import Node
-
 from action_bipedal_interface.action import DcController
-
 import RPi.GPIO as GPIO
 import time
 
-
 class DCControllerActionServer(Node):
 
-	def __init__(self):
-		super().__init__('dc_controller_action_server')
-		self._action_server = ActionServer(
-			self,
-			DcController,
-			'dc_controller',
-			self.execute_callback)
-		
-		# GPIO setup
-		self.EN_A = 22         # GPIO pin for L298N enable of Motor A
-		self.IN_1 = 26         # GPIO pin for L298N direction of Motor A
-		self.IN_2 = 6          # GPIO pin for L298N direction of Motor A
+    def __init__(self):
+        super().__init__('dc_controller_action_server')
+        self._action_server = ActionServer(
+            self,
+            DcController,
+            'dc_controller',
+            execute_callback=self.execute_callback,
+            handle_accepted_callback=self.handle_accepted_callback,
+            goal_callback=self.goal_callback,
+            cancel_callback=self.cancel_callback)
 
-		self.EN_B = 5         # GPIO pin for L298N enable of Motor A
-		self.IN_3 = 12         # GPIO pin for L298N direction of Motor A
-		self.IN_4 = 16          # GPIO pin for L298N direction of Motor A
+        # GPIO setup
+        self.EN_A = 22         # GPIO pin for L298N enable of Motor A
+        self.IN_1 = 26         # GPIO pin for L298N direction of Motor A
+        self.IN_2 = 6          # GPIO pin for L298N direction of Motor A
 
-		self.ENC_A1 = 27  # GPIO pin for encoder 1 of Motor A
-		self.ENC_A2 = 17  # GPIO pin for encoder 2 of Motor A
+        self.EN_B = 5         # GPIO pin for L298N enable of Motor A
+        self.IN_3 = 12         # GPIO pin for L298N direction of Motor A
+        self.IN_4 = 16          # GPIO pin for L298N direction of Motor A
 
-		# PID constants
-		self.kp = 5
-		self.ki = 0.6
-		self.kd = 0
+        self.ENC_A1 = 27  # GPIO pin for encoder 1 of Motor A
+        self.ENC_A2 = 17  # GPIO pin for encoder 2 of Motor A
 
-		# Variables for encoder
-		self.counts_per_rev = 1942
-		self.counter = 0
-		self.rpm = 0
-		self.last_time_enc = time.time()
+        # PID constants
+        self.kp = 5
+        self.ki = 0.6
+        self.kd = 0
 
-		# PID variables
-		self.setpoint = 0	# Speed wanted
-		self.output = 0
-		self.prev_error = 0
-		self.integral = 0
-		self.last_time = time.time()
+        # Variables for encoder
+        self.counts_per_rev = 1942
+        self.counter = 0
+        self.rpm = 0
+        self.last_time_enc = time.time()
 
-		# GPIO setup
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.EN_A, GPIO.OUT)
-		GPIO.setup(self.IN_1, GPIO.OUT)
-		GPIO.setup(self.IN_2, GPIO.OUT)
+        # PID variables
+        self.setpoint = 0	# Speed wanted
+        self.output = 0
+        self.prev_error = 0
+        self.integral = 0
+        self.last_time = time.time()
 
-		GPIO.setup(self.EN_B, GPIO.OUT)
-		GPIO.setup(self.IN_3, GPIO.OUT)
-		GPIO.setup(self.IN_4, GPIO.OUT)
+        # GPIO setup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.EN_A, GPIO.OUT)
+        GPIO.setup(self.IN_1, GPIO.OUT)
+        GPIO.setup(self.IN_2, GPIO.OUT)
 
-		GPIO.setup(self.ENC_A1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.setup(self.ENC_A2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.EN_B, GPIO.OUT)
+        GPIO.setup(self.IN_3, GPIO.OUT)
+        GPIO.setup(self.IN_4, GPIO.OUT)
 
-		self.last_state_a1 = GPIO.input(self.ENC_A1)
+        GPIO.setup(self.ENC_A1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.ENC_A2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-		# Initialize PWMA
-		self.pwmA = GPIO.PWM(self.EN_A, 1000)  # Initialize PWM on E_LEFT pin 1000Hz frequency
-		self.pwmA.start(0)
+        self.last_state_a1 = GPIO.input(self.ENC_A1)
 
-		# Initialize PWMB
-		self.pwmB = GPIO.PWM(self.EN_B, 1000)  # Initialize PWM on E_LEFT pin 1000Hz frequency
-		self.pwmB.start(0)
+        # Initialize PWMA
+        self.pwmA = GPIO.PWM(self.EN_A, 1000)  # Initialize PWM on E_LEFT pin 1000Hz frequency
+        self.pwmA.start(0)
 
-		# Initialize encoder
-		GPIO.add_event_detect(self.ENC_A1, GPIO.BOTH, callback=self.encoder_callback)
-		
+        # Initialize PWMB
+        self.pwmB = GPIO.PWM(self.EN_B, 1000)  # Initialize PWM on E_LEFT pin 1000Hz frequency
+        self.pwmB.start(0)
 
-	def encoder_callback(self, channel):
-		state_a1 = GPIO.input(self.ENC_A1)
-		state_a2 = GPIO.input(self.ENC_A2)
+        # Initialize encoder
+        GPIO.add_event_detect(self.ENC_A1, GPIO.BOTH, callback=self.encoder_callback)
+        
+        # Reference to the current goal handle
+        self.current_goal_handle = None
 
-		if state_a1 != self.last_state_a1:  # A has changed
-			if state_a1 == state_a2:
-				self.counter += 1
-			else:
-				self.counter -= 1
+    def encoder_callback(self, channel):
+        state_a1 = GPIO.input(self.ENC_A1)
+        state_a2 = GPIO.input(self.ENC_A2)
 
-		self.last_state_a1 = state_a1
-		
-		#print(f"Counter: {self.counter}")
+        if state_a1 != self.last_state_a1:  # A has changed
+            if state_a1 == state_a2:
+                self.counter += 1
+            else:
+                self.counter -= 1
 
-	def calculate_rpm(self):
-		current_time = time.time()
-		elapsed_time = current_time - self.last_time_enc
-		print(f"elapsed time: {elapsed_time}")
-		self.last_time_enc = current_time
+        self.last_state_a1 = state_a1
 
-		# Calculate RPM
-		revolutions = self.counter / self.counts_per_rev
-		print(f"counter: {self.counter}")
-		self.counter = 0  # Reset counter after calculating RPM
-		self.rpm = (revolutions / elapsed_time)*60
+    def calculate_rpm(self):
+        current_time = time.time()
+        elapsed_time = current_time - self.last_time_enc
+        self.last_time_enc = current_time
 
-	def compute_pid(self):		
-		current_time = time.time()
+        # Calculate RPM
+        revolutions = self.counter / self.counts_per_rev
+        self.counter = 0  # Reset counter after calculating RPM
+        self.rpm = (revolutions / elapsed_time) * 60
 
-		dt = current_time - self.last_time
+    def compute_pid(self):
+        current_time = time.time()
+        dt = current_time - self.last_time
 
-		self.abs_rpm = abs(self.rpm)
-		
-		error = self.setpoint - self.abs_rpm
-		self.integral += error * dt
-		derivative = (error - self.prev_error) / dt if dt > 0 else 0
-		
-		self.output = self.kp * error + self.ki * self.integral + self.kd * derivative
-		self.output = max(0, min(100, self.output))  # Clamp self.output to range 0-100
-		print(f"output: {self.output}")
-		
-		self.prev_error = error
-		self.last_time = current_time
+        self.abs_rpm = abs(self.rpm)
+        error = self.setpoint - self.abs_rpm
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt if dt > 0 else 0
 
-	def advance(self):
-		self.pwmA.ChangeDutyCycle(self.output)
-		GPIO.output(self.IN_1, GPIO.HIGH)
-		GPIO.output(self.IN_2, GPIO.LOW)
+        self.output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        self.output = max(0, min(100, self.output))  # Clamp self.output to range 0-100
 
-		self.pwmB.ChangeDutyCycle(self.output)
-		GPIO.output(self.IN_3, GPIO.HIGH)
-		GPIO.output(self.IN_4, GPIO.LOW)
+        self.prev_error = error
+        self.last_time = current_time
 
-	def back(self):
-		self.pwmA.ChangeDutyCycle(self.output)
-		GPIO.output(self.IN_1, GPIO.LOW)
-		GPIO.output(self.IN_2, GPIO.HIGH)
+    def advance(self):
+        self.pwmA.ChangeDutyCycle(self.output)
+        GPIO.output(self.IN_1, GPIO.HIGH)
+        GPIO.output(self.IN_2, GPIO.LOW)
 
-		self.pwmB.ChangeDutyCycle(self.output)
-		GPIO.output(self.IN_3, GPIO.LOW)
-		GPIO.output(self.IN_4, GPIO.HIGH)
+        self.pwmB.ChangeDutyCycle(self.output)
+        GPIO.output(self.IN_3, GPIO.HIGH)
+        GPIO.output(self.IN_4, GPIO.LOW)
 
-	def stop(self):
-		self.pwmA.ChangeDutyCycle(0)
-		GPIO.output(self.IN_1, GPIO.LOW)
-		GPIO.output(self.IN_2, GPIO.LOW)
+    def back(self):
+        self.pwmA.ChangeDutyCycle(self.output)
+        GPIO.output(self.IN_1, GPIO.LOW)
+        GPIO.output(self.IN_2, GPIO.HIGH)
 
-		self.pwmB.ChangeDutyCycle(0)
-		GPIO.output(self.IN_3, GPIO.LOW)
-		GPIO.output(self.IN_4, GPIO.LOW)
-	
-	def destroy(self):
-		self.pwmA.stop()
-		self.pwmB.stop()
-		GPIO.cleanup()
+        self.pwmB.ChangeDutyCycle(self.output)
+        GPIO.output(self.IN_3, GPIO.LOW)
+        GPIO.output(self.IN_4, GPIO.HIGH)
 
-	def execute_callback(self, goal_handle):
-		# PID variables
-		self.integral = 0
+    def stop(self):
+        self.pwmA.ChangeDutyCycle(0)
+        GPIO.output(self.IN_1, GPIO.LOW)
+        GPIO.output(self.IN_2, GPIO.LOW)
 
-		self.get_logger().info('Executing goal...')
+        self.pwmB.ChangeDutyCycle(0)
+        GPIO.output(self.IN_3, GPIO.LOW)
+        GPIO.output(self.IN_4, GPIO.LOW)
+    
+    def destroy(self):
+        self.pwmA.stop()
+        self.pwmB.stop()
+        GPIO.cleanup()
 
-		feedback_msg = DcController.Feedback()
-		feedback_msg.current_rpm = int(self.rpm)
+    def handle_accepted_callback(self, goal_handle):
+        self.get_logger().info('Received new goal')
+        
+        # Cancel the current goal if it exists
+        if self.current_goal_handle:
+            self.get_logger().info('Cancelling current goal')
+            self.current_goal_handle.abort()
 
-		command = goal_handle.request.goal
-		self.setpoint = abs(command)
+        # Accept the new goal
+        self.current_goal_handle = goal_handle
+        goal_handle.execute()
 
-		while self.setpoint != self.output :
+    def goal_callback(self, goal_request):
+        return rclpy.action.GoalResponse.ACCEPT
 
-			self.calculate_rpm()
-			print(f"Motor RPM: {self.rpm}")
+    def cancel_callback(self, goal_handle):
+        self.get_logger().info('Cancelling goal')
+        return CancelResponse.ACCEPT
 
-			self.compute_pid()  # Compute the PID output
+    def execute_callback(self, goal_handle):
+        # PID variables
+        self.integral = 0
 
-			if int(command) > 0 : self.advance()
-			else : self.back()
+        self.get_logger().info('Executing goal...')
 
-			feedback_msg.current_rpm = int(self.rpm)
-			self.get_logger().info('Feedback: {0}'.format(feedback_msg.current_rpm))
+        feedback_msg = DcController.Feedback()
+        feedback_msg.current_rpm = int(self.rpm)
 
-			goal_handle.publish_feedback(feedback_msg)
+        command = goal_handle.request.goal
+        self.setpoint = abs(command)
 
-		result = DcController.Result()
-		result.final_rpm = feedback_msg.current_rpm
-		return result
+        while self.setpoint != self.output and rclpy.ok():
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().info('Goal canceled')
+                self.stop()
+                return DcController.Result()
 
+            self.calculate_rpm()
+            self.compute_pid()  # Compute the PID output
+
+            if int(command) > 0:
+                self.advance()
+            else:
+                self.back()
+
+            feedback_msg.current_rpm = int(self.rpm)
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.current_rpm))
+            goal_handle.publish_feedback(feedback_msg)
+
+        result = DcController.Result()
+        result.final_rpm = feedback_msg.current_rpm
+        goal_handle.succeed()
+        return result
 
 def main(args=None):
-	rclpy.init(args=args)
+    rclpy.init(args=args)
 
-	dc_controller_action_server = DCControllerActionServer()
+    dc_controller_action_server = DCControllerActionServer()
 
-	try:
-		rclpy.spin(dc_controller_action_server)
-	except KeyboardInterrupt:
-		pass
-	finally:
-		dc_controller_action_server.destroy()
-		rclpy.shutdown()
+    try:
+        rclpy.spin(dc_controller_action_server)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        dc_controller_action_server.destroy()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
-	main()
+    main()
