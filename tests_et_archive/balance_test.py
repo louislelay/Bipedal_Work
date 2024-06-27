bc7f0e41881864845b54b6d84abc82d438ebfda5
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-28 00:02:31 +0200

    Update balance_test.py

--- tests_et_archive/balance_test.py
+++ tests_et_archive/balance_test.py
@@ -81,9 +81,9 @@ def set_servo_angle(pwm, angle):
 	#pwm.ChangeDutyCycle(0)
 
 def PID(input, I, prev_input):
-	Kp = 0.5
+	Kp = 1
 	Ki = 0
-	Kd = 0.1
+	Kd = 0
 
 	P = Kp * (float(input))
 	I += Ki * (float(input))
