c81e94ece1fa84d00ac4d4c75ee22ceec61e277c
Louis LE LAY <le.lay.louis@gmail.com>
2024-06-27 22:54:18 +0200

    Update balance_test.py

--- tests_et_archive/balance_test.py
+++ tests_et_archive/balance_test.py
@@ -111,7 +111,7 @@ try:
 			set_servo_angle(servo_pwm[i], angless[i])
 
 		# Control DC motors based on tilt
-		if roll < -10:
+		if roll < -5:
 			control_dc_motor(motor_pins, 100, 'forward')
 		elif roll > -10:
 			control_dc_motor(motor_pins, 100, 'backward')
